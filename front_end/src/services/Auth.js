5fe3c71a2721f31d85ae8a1cc1fbd6c436c82ed3
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2020-05-24 10:36:46 -0300

    feat: finalizacao de servico de login

--- front_end/src/services/Auth.js
+++ front_end/src/services/Auth.js
@@ -1,14 +1,27 @@
 import Axios from 'axios';
-import { API } from '../config';
+import { API, TOKEN_KEY, USER_KEY } from '../config';
+import { UserSession } from '../model/UserSession';
 
 export default {
   async login(user) {
     console.log(user);
     const response = await Axios.post(`${API}/auth/login`, user);
     console.log(response);
+    localStorage.setItem(TOKEN_KEY, response.data.accessToken);
+    const userResponse = response.data.user;
+    localStorage.setItem(
+      USER_KEY,
+      JSON.stringify(
+        new UserSession(
+          userResponse.name,
+          userResponse.email,
+          userResponse.type
+        )
+      )
+    );
   },
 
-  async users() {
-    console.log(await Axios.post(`${API}/teacher`, { name: 'leanderson' }));
+  isAuthenticated() {
+    return localStorage.getItem(TOKEN_KEY) !== null;
   },
 };
