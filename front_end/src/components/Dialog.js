a1aa4841b7ec33ea2e05fd76c0a0e11d02a58f05
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2020-05-24 13:20:55 -0300

    feat: janela de dialog dinamica

--- front_end/src/components/Dialog.js
+++ front_end/src/components/Dialog.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { Dialog as MUIDialog } from '@material-ui/core';
+import { Dialog as MUIDialog, Grid, makeStyles } from '@material-ui/core';
+
+export const useStyle = makeStyles(() => ({
+  modal: {
+    width: 200,
+    height: 200,
+  },
+}));
 
 const Dialog = (props) => {
   const { children } = props;
   const { open } = props;
+  const { style } = props;
+  const classe = useStyle();
 
-  return <MUIDialog open={open}>{children}</MUIDialog>;
+  return (
+    <MUIDialog open={open}>
+      <Grid
+        container
+        alignItems="center"
+        direction="column"
+        className={style || classe.modal}
+      >
+        {children}
+      </Grid>
+    </MUIDialog>
+  );
 };
 
 export default Dialog;
