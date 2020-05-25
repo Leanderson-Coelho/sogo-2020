18bfa286da4c13014eb43fc1f084dfe5086a8c08
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2020-05-25 18:46:47 -0300

    feat: DTO para teach

---
+++ front_end/src/model/TeacherDTO.js
@@ -0,0 +1,8 @@
+export class TeacherDTO {
+  constructor(name, email, registration, password) {
+    this.name = name;
+    this.email = email;
+    this.registration = registration;
+    this.password = password;
+  }
+}
