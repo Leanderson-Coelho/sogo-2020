24098f96f11d0ae701654f5aaf7fdd13e44f2b98
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2020-05-26 12:14:12 -0300

    feat: tema mui para usuário participant

---
+++ front_end/src/styles/participantTheme.js
@@ -0,0 +1,18 @@
+import { createMuiTheme } from '@material-ui/core';
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#F9A826',
+    },
+    secondary: {
+      main: '#6C63FF',
+    },
+    success: {
+      main: '#428a04',
+    },
+    background: {
+      default: '#E6E6E6',
+    },
+  },
+});
